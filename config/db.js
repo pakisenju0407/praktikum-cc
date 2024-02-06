bacf6ff5796a1d2d819711f31c17fb1b395b6a60
Fahki Rohandi <fahkirohandi07@gmail.com>
2024-02-06 20:21:53 +0800

    first commit

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
 	host: 'sql.freedb.tech',
 	dialect: 'mysql',
-    username: 'freedb_pakisenju',
-    password: 'Fpr&Wt2#Kaau*23',
-    database: 'freedb_sequelize-todo'
+    username: 'freedb_pakirohandi',
+    password: 'qq@H9MYjp$ru6?G',
+    database: 'freedb_todolist_sequalize'
 });
 
 module.exports = sequelize
